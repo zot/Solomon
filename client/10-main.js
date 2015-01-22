5838077b3324aeda5da33c9ba1139a7b2a3a2327
Gilan Israel <gslisrael@gmail.com>
2015-01-22 21:31:09 +0200

    working on reveal map

--- client/10-main.js
+++ client/10-main.js
@@ -4,13 +4,12 @@ var Gui = Solomon.Gui = {};
 var speak = Solomon.Middleware.speak;
 var teamChat = Solomon.Middleware.teamChat;
 var offerMap = Solomon.Middleware.offerMap = {};
+var shareMaps = Solomon.Middleware.shareMaps;
 
 Gui.g = {
 	DEFAULT_CHAT_TEXT : "Type chat message here...",
 	OFFSET_X : 7,
 	OFFSET_Y : 5,
-	PLAYER_WIDTH: 32,
-	PLAYER_HEIGHT: 32,
 	revealedMap : []
 };
 
@@ -36,17 +35,20 @@ Gui.sendMessage = function (target) {
 
 Gui.playerMoved = function (player, map) {
 	var playerX, playerY, startX, startY, topCornerX, topCornerY, i, j, bottomCornerX, bottomCornerY;
-	playerX = player.X;
-	playerY = player.Y;
+	if (typeof player.x === 'undefined') {
+		return;
+	}
+	playerX = player.x;
+	playerY = player.y;
 	
 	topCornerX = playerX - Gui.g.OFFSET_X;
 	topCornerY = playerY - Gui.g.OFFSET_Y;
-	bottomCornerX = playerX + Gui.g.OFFSET_X + Gui.g.PLAYER_WIDTH;
-	bottomCornerY = playerY + Gui.g.OFFSET_Y + Gui.g.PLAYER_HEIGHT;
+	bottomCornerX = playerX + Gui.g.OFFSET_X + 1;
+	bottomCornerY = playerY + Gui.g.OFFSET_Y + 1;
 	
 	for (j = topCornerY; j < bottomCornerY; j++) {
 		for (i = topCornerX; i < bottomCornerX; i++) {
-			revealedMap[j][i] = map[i][j];
+			Gui.g.revealedMap[j][i] = map[i][j];
 			Gui.context.fillRect(i * 5, j * 5, 5, 5);
 		}
 	}
@@ -82,9 +84,15 @@ function changePlayer(changeType, item) {
 	switch (changeType) {
 	case 'added':
 		$("#localViewInner").append("<div id='" + item._id + "' class='character' style='left: 224px; top: 160px; background-color: orange'>" + item.username + "</div>");
+		if (item._id === Meteor.userId()) {
+			//Gui.playerMoved(Meteor.user(), Solomon.maze.findOne("world").map);
+		}
 		break;
 	case 'changed':
 		$("##{item._id}");
+		if (item._id === Meteor.userId()) {
+			Gui.playerMoved(Meteor.user(), Solomon.maze.findOne("world").map);
+		}
 		break;
 	case 'removed':
 		$("##{item._id}").remove();
@@ -108,8 +116,29 @@ Gui.handleDataChange = function (changeType, item) {
 Gui.onLogin = function () {
 };
 
+Gui.receiveMapShareRequest = function (user) {
+	$(".shareName").text(user.username);
+	$("#dialog-agree-to-share").dialog({
+		resizable: false,
+		height: 200,
+		modal: true,
+		buttons: {
+			"Agree": function() {
+				shareMaps(user);
+				console.log("yes");
+				$( this ).dialog( "close" );
+			},
+			Cancel: function() {
+				console.log("no");
+				$( this ).dialog( "close" );
+			}
+		},
+		width: 600
+	});	
+};
+
 Gui.askToShareMaps = function (event, ui) {
-	$("#shareName").text(ui.target.text());
+	$(".shareName").text(ui.target.text());
 	$("#dialog-confirm-share").dialog({
 		resizable: false,
 		height: 200,
@@ -150,6 +179,7 @@ $(document).keydown(function (event) {
 });
 
 $(document).ready(function () {
+	var map, i, j;
 	  $("#chatTabs").tabs();
 	  $(".chatInput").val(Gui.g.DEFAULT_CHAT_TEXT);
 	  $(".chatInput").focus(function () {
@@ -179,5 +209,14 @@ $(document).ready(function () {
 	  Gui.canvasElem.attr("width", Gui.canvasElem.width());
 	  Gui.context = Gui.canvasElem[0].getContext("2d");
 	  Solomon.Gui.context.fillStyle = '#000';
+	map = Solomon.maze.findOne("world").map
+	Gui.g.revealedMap = [];
+	for (i = 0; i < map.length; i++) {
+		Gui.g.revealedMap[i] = [];
+		for (j = 0; j < map[i].length; j++) {
+			Gui.g.revealedMap[i][j] = null;
+		}
+	}
+	
 });
 })();
